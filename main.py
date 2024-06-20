345e2ba75f8e53cbc042722de0da9cc51b0db797
Annie Chakraborty <46227364+anniechakraborty@users.noreply.github.com>
2024-06-20 18:51:35 +0200

    records inserted to db

--- main.py
+++ main.py
@@ -20,29 +20,50 @@ def read_csv_to_dict(file_path, key_column_index=0):
     
     return data_dict
 
+def create_tmp_csv():
+    # Code to create a temp CSV
+    # Define the header and data
+    header = ['book_id', 'authors', 'title', 'average_rating', 'image_url']
+    data = [
+        [1, 'Suzanne Collins', 'The Hunger Games (The Hunger Games, #1)', 4.34, 'https://images.gr-assets.com/books/1447303603m/2767052.jpg'],
+        [2, 'J.K. Rowling, Mary GrandPré', "Harry Potter and the Sorcerer's Stone (Harry Potter, #1)", 4.44, 'https://images.gr-assets.com/books/1474154022m/3.jpg'],
+        [3, 'Stephenie Meyer', 'Twilight (Twilight, #1)', 3.57, 'https://images.gr-assets.com/books/1361039443m/41865.jpg'],
+        [4, 'Harper Lee', 'To Kill a Mockingbird', 4.25, 'https://images.gr-assets.com/books/1361975680m/2657.jpg']
+    ]
+
+    # Specify the file name
+    filename = 'asset/books_temp.csv'
+
+    # Write data to the CSV file
+    with open(filename, 'w', newline='', encoding='utf-8')  as file:
+        writer = csv.writer(file)
+        writer.writerow(header)  # Write the header
+        writer.writerows(data)   # Write the data
+
+    print(f"CSV file '{filename}' created successfully.")
+
 def encode_to_xml(json_data):
     #write here
     return ""
+
+def insert_records(collection):
+    file_path = 'asset/books.csv'
+    # create_tmp_csv()
+    data = read_csv_to_dict(file_path)
+    data_list = list(data.values())
+    result = collection.insert_many(data_list)
+    print("Records inserted successfully : ", result)
 
 if __name__ == '__main__':
     uri = "mongodb://localhost:27017"
     client = MongoClient(uri)
     db = client['BookFinder']
     collection = db['Books']
-    document = {
-        "title": "1984",
-        "author": "George Orwell",
-        "year_published": 1949
-    }
 
-    # Insert the document into the collection
-    result = collection.insert_one(document)
-    print(f"Document inserted with _id: {result.inserted_id}")
+    # insert_records(collection)
+
 
     # Example operation: Find a document
-    found_document = collection.find({"title": "1984"})
-    print("Found document:", found_document)
-
-    file_path = 'asset/books.csv'
-    data = read_csv_to_dict(file_path)
+    # found_document = collection.find({"title": "1984"})
+    # print("Found document:", found_document)
 
