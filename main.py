a9d48e41a24b2dc5ab0c8417e41cc5ca2e18dd70
Annie Chakraborty <46227364+anniechakraborty@users.noreply.github.com>
2024-06-20 17:55:51 +0200

    connected to mongodb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from pymongo import MongoClient
 
 def read_csv_to_dict(file_path, key_column_index=0):
     """
@@ -13,13 +14,35 @@ def read_csv_to_dict(file_path, key_column_index=0):
     with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         headers = next(reader)  # Read the header row
-        # print(headers)
         for row in reader:
             key = row[key_column_index]
             data_dict[key] = {headers[i]: row[i] for i in range(len(headers))}
     
     return data_dict
 
-file_path = 'asset/books.csv'
-data = read_csv_to_dict(file_path)
-# print(data)
+def encode_to_xml(json_data):
+    #write here
+    return ""
+
+if __name__ == '__main__':
+    uri = "mongodb://localhost:27017"
+    client = MongoClient(uri)
+    db = client['BookFinder']
+    collection = db['Books']
+    document = {
+        "title": "1984",
+        "author": "George Orwell",
+        "year_published": 1949
+    }
+
+    # Insert the document into the collection
+    result = collection.insert_one(document)
+    print(f"Document inserted with _id: {result.inserted_id}")
+
+    # Example operation: Find a document
+    found_document = collection.find({"title": "1984"})
+    print("Found document:", found_document)
+
+    file_path = 'asset/books.csv'
+    data = read_csv_to_dict(file_path)
+
